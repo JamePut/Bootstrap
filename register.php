07a069b69fb74f5ba3bd04f63821cb925fc45c69
JamePut <dutchmillblu__@hotmail.com>
2022-11-02 11:52:28 +0700

    Day4

--- register.php
+++ register.php
@@ -24,6 +24,16 @@
             <div class="col-md-3"></div>
             <div class="col-md-6">
                 <div class="card text-dark bg-white border-primary">
+                    <?php 
+                        if(isset($_SESSION['add_login'])){
+                            if($_SESSION['add_login']=='error'){
+                                echo "<div class='alert alert-danger'>ชื่อบัญชีซ้ำหรือฐานข้อมูลมีปัญหา</div>";
+                            }else{
+                                echo "<div class='alert alert-success'>เพิ่มบัญชีเรียบร้อยแล้ว</div>";
+                            }
+                            unset($_SESSION['add_login']);
+                        }
+                    ?>
                     <div class="card-header bg-primary text-white">กรอกข้อมูล</div>
                     <div class="card-body">
                         <form action="register_save.php" method="post">
